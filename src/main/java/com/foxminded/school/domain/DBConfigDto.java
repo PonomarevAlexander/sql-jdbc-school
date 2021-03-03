cff516754aec321ec727496387ccbad2b14770ad
Alexander Ponomarev <ponomarev.alex.1992@gmail.com>
2021-03-03 22:06:02 +0300

    add database connection config dto

---
+++ src/main/java/com/foxminded/school/domain/DBConfigDto.java
@@ -0,0 +1,38 @@
+package com.foxminded.school.domain;
+
+public class DBConfigDto {
+    
+    private String url;
+    private String user;
+    private String password;
+
+    public DBConfigDto(String url, String user, String password) {
+        this.url = url;
+        this.user = user;
+        this.password = password;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
