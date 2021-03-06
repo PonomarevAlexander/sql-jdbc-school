3271b3badeaaf10405b06fb44f7e872c4c1d5fca
Alexander Ponomarev <ponomarev.alex.1992@gmail.com>
2021-03-06 23:24:48 +0300

    refactoring generator() logic

--- src/main/java/com/foxminded/school/domain/factory/CourseGenerator.java
+++ src/main/java/com/foxminded/school/domain/factory/CourseGenerator.java
@@ -1,26 +1,28 @@
 package com.foxminded.school.domain.factory;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.foxminded.school.domain.models.Course;
 
-public class CourseFactory implements ModelFactory<List<Course>> {
+public class CourseGenerator implements ModelGenerator<List<Course>> {
 
     @Override
     public List<Course> generate(int counter) {
+        Path courseData = Paths.get("C:\\Projects\\sql-jdbc-school\\src\\main\\resources\\course_data.txt");
         List<Course> coursesList = new ArrayList<>();
-        coursesList.add(new Course("Algebra"));
-        coursesList.add(new Course("Literature"));
-        coursesList.add(new Course("History"));
-        coursesList.add(new Course("Physics"));
-        coursesList.add(new Course("Music"));
-        coursesList.add(new Course("Biology"));
-        coursesList.add(new Course("Geography"));
-        coursesList.add(new Course("Drawing"));
-        coursesList.add(new Course("English"));
-        coursesList.add(new Course("Science"));
+        
+        try (Stream<String> fileStream = Files.lines(courseData)){
+            coursesList = fileStream.limit(counter).map(line -> new Course(line)).collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return coursesList;
     }
-
 }
