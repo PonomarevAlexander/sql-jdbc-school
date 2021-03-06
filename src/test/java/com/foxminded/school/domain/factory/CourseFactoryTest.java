82f3813ccee30906338bca6ef14ba37bab67a1f8
Alexander Ponomarev <ponomarev.alex.1992@gmail.com>
2021-03-06 23:24:08 +0300

    refactoring course generator test

--- src/test/java/com/foxminded/school/domain/factory/CourseFactoryTest.java
+++ src/test/java/com/foxminded/school/domain/factory/CourseFactoryTest.java
@@ -1,38 +1,23 @@
 package com.foxminded.school.domain.factory;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.ArrayList;
 import java.util.List;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import com.foxminded.school.domain.models.Course;
 
 class CourseFactoryTest {
 
-    private CourseFactory cFactory;
+    private CourseGenerator courseGenerator;
     
     @BeforeEach
     void init() {
-        cFactory = new CourseFactory();
+        courseGenerator = new CourseGenerator();
     }
     
     @Test
     void testGenerate() {
-        List<Course> coursesList = new ArrayList<>();
-        coursesList.add(new Course("Algebra"));
-        coursesList.add(new Course("Literature"));
-        coursesList.add(new Course("History"));
-        coursesList.add(new Course("Physics"));
-        coursesList.add(new Course("Music"));
-        coursesList.add(new Course("Biology"));
-        coursesList.add(new Course("Geography"));
-        coursesList.add(new Course("Drawing"));
-        coursesList.add(new Course("English"));
-        coursesList.add(new Course("Science"));
-        List<Course> actual = cFactory.generate(10);
+        List<Course> actual = courseGenerator.generate(10);
         assertEquals(10, actual.size());
     }
 }
